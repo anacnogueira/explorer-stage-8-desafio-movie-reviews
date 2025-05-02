c99c7f7e5d65f8c3a52499d77e9d5f0c4ec0da2b
Ana Claudia <anacnogueira@gmail.com>
2025-05-02 10:37:20 -0300

    Filter movie notes by title e/or tags

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -3,14 +3,41 @@ import { connection } from "../database/knex/index.js";
 export class MovieNotesController {
   async index(request, response) {
     const { user_id } = request.params;
-    let notes;
+    const { title, tags } = request.query;
 
-    notes = await connection("movie_notes")
-      .select("id", "title", "description", "rating")
-      .where({ user_id })
-      .orderBy("title");
+    const notes = await connection("movie_notes")
+      .select(
+        "movie_notes.id",
+        "movie_notes.title",
+        "movie_notes.description",
+        "movie_notes.rating"
+      )
+      .where("movie_notes.user_id", user_id)
+      .orderBy("movie_notes.title")
+      .modify(function (queryBuilder) {
+        if (title) {
+          queryBuilder.whereLike("movie_notes.title", `%${title}%`);
+        }
 
-    return response.json(notes);
+        if (tags) {
+          const filterTags = tags.split(",").map((tag) => tag.trim());
+          queryBuilder
+            .innerJoin("movie_tags", "movie_tags.note_id", "movie_notes.id")
+            .whereIn("movie_tags.name", filterTags);
+        }
+      });
+
+    const userTags = await connection("movie_tags").where({ user_id });
+    const notesWithTags = notes.map((note) => {
+      const noteTags = userTags.filter((tag) => tag.note_id == note.id);
+
+      return {
+        ...note,
+        tags: noteTags,
+      };
+    });
+
+    return response.json(notesWithTags);
   }
 
   async create(request, response) {
